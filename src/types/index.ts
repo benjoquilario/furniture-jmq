0c07b4557627fcff53e5cfbd5c148a7c601a2e41
johnjube <146010515+johnjube@users.noreply.github.com>
2025-06-19 10:48:47 +0000

    feat: add VideoData interface to define video attributes and metadata

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,3 +16,22 @@ export interface FurnitureItem extends IFurnitureItem {
   images: FurnitureImage[]
   seller: User
 }
+
+export interface VideoData {
+  id: string
+  title: string
+  description: string
+  videoUrl: string
+  thumbnailUrl: string
+  author: {
+    name: string
+    avatar: string
+    username: string
+  }
+  stats: {
+    likes: number
+    comments: number
+    shares: number
+  }
+  tags: string[]
+}
